9ff4476fbbc613972c32d9b2dd6ced659685f564
barnold8 <bandslover@outlook.com>
2026-01-21 03:55:41 +0000

    Add config.fish

---
+++ config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+
+alias cls='clear'
+alias rf='source ~/.config/fish/config.fish' 
+alias fish_config='vim ~/.config/fish/config.fish'
+alias falias="cat ~/.config/fish/config.fish"
+alias i3_config="vim ~/.config/i3/config"
+
